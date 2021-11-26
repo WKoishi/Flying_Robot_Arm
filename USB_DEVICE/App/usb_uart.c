8d3a3f91a1bf97ae9a9009eaa0b52e2863f8e214
WKoishi <intelwzt@outlook.com>
2021-11-26 20:24:39 +0800

    initialize project

---
+++ USB_DEVICE/App/usb_uart.c
@@ -0,0 +1,18 @@
+#include "usb_uart.h"
+#include "usbd_cdc_if.h"
+
+extern uint8_t UserRxBufferFS[APP_RX_DATA_SIZE];
+extern uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
+
+void usb_printf(const char *format, ...)
+{
+    va_list args;
+    uint32_t length = 0;
+ 
+    va_start(args, format);
+    length = vsnprintf((char *)UserTxBufferFS, APP_TX_DATA_SIZE, (char *)format, args);
+    va_end(args);
+    CDC_Transmit_FS(UserTxBufferFS, length);
+}
+
+
---
+++ USB_DEVICE/App/usb_uart.h
@@ -0,0 +1,13 @@
+#ifndef __USB_UART_H
+#define __USB_UART_H
+
+#include "stdarg.h"
+#include "stdint.h"
+
+void usb_printf(const char *format, ...);
+
+#endif //usb_uart.h
+
+
+
+
