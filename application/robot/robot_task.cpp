fce35b0a8fe0a7c6c9e7c28beb9a80b63e771ac4
WKoishi <intelwzt@outlook.com>
2022-01-26 14:14:42 +0800

    global: add DSP library

--- application/robot/robot_task.cpp
+++ application/robot/robot_task.cpp
@@ -3,6 +3,7 @@
 #include "servo_ctrl.h"
 #include "usb_uart.h"
 #include "joint.hpp"
+#include "arm_math.h"
 
 ServoObject servo_1(0X01);
 ServoObject servo_2(0X02);
@@ -18,37 +19,40 @@ extern "C" void servo_ctrl_task(void const * argument)
     int16_t posi_target = 0;
     int8_t reserve_flag = 1;
     
-    servo_1.export_read_state();
-    posi_target = servo_1.get_position();
+    int i = 0;
+    float sinin = 0;
     
-    servo_1.set_accelerate(20);
+//    servo_1.export_read_state();
+//    posi_target = servo_1.get_position();
+//    
+//    servo_1.set_accelerate(20);
     
     for (;;)
     {
         osDelay(20);
         
-        get_time_period(&servo_time);
-        ret_val = servo_1.set_position(posi_target);
-        get_time_period(&servo_time);
-//        if (ret_val)
-//            usb_printf("Y1 %d,%d,%f,%f,%d,%f,%f\r\n", servo_1.position, servo_1.velocity, 
-//                servo_1.load_ratio, servo_1.voltage, servo_1.temperature, servo_1.current, servo_time.Time_Delta);
-//        else
-//            usb_printf("N1 %d,%d,%f,%f,%d,%f,%f\r\n", servo_1.position, servo_1.velocity, 
-//                servo_1.load_ratio, servo_1.voltage, servo_1.temperature, servo_1.current, servo_time.Time_Delta);
-        
-        posi_target += 10 * reserve_flag;
-        
-        if (posi_target > 4000)
-        {
-            reserve_flag = -1;
-            posi_target = 4000;
-        }
-        else if (posi_target < 0)
-        {
-            reserve_flag = 1;
-            posi_target = 0;
-        }
+//        get_time_period(&servo_time);
+//        ret_val = servo_1.set_position(posi_target);
+//        get_time_period(&servo_time);
+////        if (ret_val)
+////            usb_printf("Y1 %d,%d,%f,%f,%d,%f,%f\r\n", servo_1.position, servo_1.velocity, 
+////                servo_1.load_ratio, servo_1.voltage, servo_1.temperature, servo_1.current, servo_time.Time_Delta);
+////        else
+////            usb_printf("N1 %d,%d,%f,%f,%d,%f,%f\r\n", servo_1.position, servo_1.velocity, 
+////                servo_1.load_ratio, servo_1.voltage, servo_1.temperature, servo_1.current, servo_time.Time_Delta);
+//        
+//        posi_target += 10 * reserve_flag;
+//        
+//        if (posi_target > 4000)
+//        {
+//            reserve_flag = -1;
+//            posi_target = 4000;
+//        }
+//        else if (posi_target < 0)
+//        {
+//            reserve_flag = 1;
+//            posi_target = 0;
+//        }
 
         
 //        get_time_period(&servo_time);
@@ -62,6 +66,16 @@ extern "C" void servo_ctrl_task(void const * argument)
 //                servo_2.load_ratio, servo_2.voltage, servo_2.temperature, servo_2.current, servo_time.Time_Delta);
         
         //usb_printf("%d %d", sizeof(*servo_1.bus_manager), sizeof(servo_1));
+
+        get_time_period(&servo_time);
+        for (i = 0; i < 2000; i++)
+        {
+            arm_cos_f32(sinin);
+            sinin += 0.5;
+        }
+        get_time_period(&servo_time);
+        sinin = 1.87324;
+        usb_printf("%f", servo_time.Time_Delta);
             
     }
 }
